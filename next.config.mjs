49d208be80e3e6c06673832ab6f72f4ef88ce5cb
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-10-10 17:06:32 -0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['img.clerk.com'],  // Add the Clerk image domain here
+  },
 };
 
 export default nextConfig;
