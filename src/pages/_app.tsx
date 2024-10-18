757d26a4af6f407d6bda8661f214d0f07bfd25ba
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-10-18 13:42:22 -0500

    temp disable auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import Layout from '../components/layout';
-import { ClerkProvider } from "@clerk/nextjs";
 import '@/styles/globals.css';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ClerkProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
