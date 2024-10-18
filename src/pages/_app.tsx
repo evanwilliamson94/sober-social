01efd5036373d33fdd40905d47d7308149c09465
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-10-18 14:21:14 -0500

    d

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from '../components/layout';
+import { ClerkProvider } from "@clerk/nextjs";
 import '@/styles/globals.css';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ClerkProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ClerkProvider>
   );
 }
